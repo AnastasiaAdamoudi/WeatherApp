94ab52eee95811f800673851803163a25f5a70d6
percygr <percyg@gmail.com>
2023-05-26 10:29:17 +0100

    directory structure and react install

---
+++ weather-app/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Results from './components/Results';
+import SearchBar from './components/SearchBar';
+
+function App() {
+  return (
+    <div className="App">
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
